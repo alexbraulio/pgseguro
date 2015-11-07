f576e36168cd09c8433c9b4661b305ddc064d597
alexbraulio <worldvision3d@gmail.com>
2015-11-07 18:20:21 -0200

    Signed-off-by: alexbraulio <worldvision3d@gmail.com>

---
+++ mana.php
@@ -0,0 +1,39 @@
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+    rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css"
+    rel="stylesheet" type="text/css">
+  </head>
+  
+  <body>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/alex.jpg"
+            class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <h1>Lazer</h1>
+            <h3>Pitura clássica</h3>
+            <p>Arte feita com algodão, foram gasto aproximadamente 16 horas de trabalho.</p>
+             <!-- INICIO FORMULARIO BOTAO PAGSEGURO -->
+<form action="https://pagseguro.uol.com.br/checkout/v2/cart.html?action=add" method="post">
+<!-- NÃO EDITE OS COMANDOS DAS LINHAS ABAIXO -->
+<input type="hidden" name="itemCode" value="688C4E264747A035549DAFB03D6DE3A3" />
+<input type="image" src="https://p.simg.uol.com.br/out/pagseguro/i/botoes/pagamentos/209x48-comprar-cinza-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
+</form>
+<!-- FINAL FORMULARIO BOTAO PAGSEGURO -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+</html>
