da794c7ee4c0a781e6a244409ea7e51b5020fe39
alexbraulio <worldvision3d@gmail.com>
2015-11-08 20:55:06 -0200

    Signed-off-by: alexbraulio <worldvision3d@gmail.com>

--- pg.php
+++ pg.php
@@ -53,6 +53,14 @@
         </div>
       </div>
     </div>
+    <!-- INICIO FORMULARIO BOTAO PAGSEGURO -->
+<form action="https:///sandbox.pagseguro.uol.com.br/checkout/v2/donation.html" method="post">
+<!-- NÃO EDITE OS COMANDOS DAS LINHAS ABAIXO -->
+<input type="hidden" name="currency" value="BRL" />
+<input type="hidden" name="receiverEmail" value="worldvision3d@gmail.com" />
+<input type="image" src="https://p.simg.uol.com.br/out/pagseguro/i/botoes/doacoes/209x48-doar-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
+</form>
+<!-- FINAL FORMULARIO BOTAO PAGSEGURO -->
   </body>
 
 </html>
