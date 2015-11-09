e9e193b10362e5339011982e1a229e95df595e7e
alexbraulio <worldvision3d@gmail.com>
2015-11-08 23:19:02 -0200

    Signed-off-by: alexbraulio <worldvision3d@gmail.com>

--- pg.php
+++ pg.php
@@ -29,6 +29,7 @@
           <div class="col-md-12">
             <h1 class="text-center text-primary">Team</h1>
             <p class="text-center">We are a group of skilled individuals.</p>
+
           </div>
         </div>
         <div class="row">
@@ -37,6 +38,13 @@
             class="center-block img-circle img-responsive">
             <h3 class="text-center">John Doe</h3>
             <p class="text-center">Developer</p>
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="W7YLVJX298NM4">
+<input type="image" src="https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - A maneira fácil e segura de enviar pagamentos online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/pt_BR/i/scr/pixel.gif" width="1" height="1">
+</form>
+
           </div>
           <div class="col-md-4">
             <img src="http://pingendo.github.io/pingendo-bootstrap/assets/user_placeholder.png"
